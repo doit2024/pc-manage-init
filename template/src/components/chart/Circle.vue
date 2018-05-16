e2bc2192b879d21d8e6907d0a4adf103218b8d3e
niuDazhuang <dengwm666@icloud.com>
2018-05-16 17:03:43 +0800

    Create Circle.vue

---
+++ template/src/components/chart/Circle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="height:400px;"></div>
+</template>
+
+<script>
+import Highcharts from 'highcharts'
+import { setOptionsCircle } from './genOptions'
+export default {
+  props: {
+    options: Array,
+    colors: Array
+  },
+  watch: {
+    options: 'init'
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      /* eslint-disable no-new */
+      new Highcharts.Chart(this.$el, setOptionsCircle({data: this.options, colors: this.colors}))
+    }
+  }
+}
+</script>
